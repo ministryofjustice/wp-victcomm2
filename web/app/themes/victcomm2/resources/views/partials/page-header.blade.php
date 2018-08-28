5be69358405202a974ef352cd03338a83d34db9a
James Devon <james.devon@digital.justice.gov.uk>
2018-08-28 17:22:41 +0100

    Page header template no longer in wrapped in a bootstrap row.

--- web/app/themes/victcomm2/resources/views/partials/page-header.blade.php
+++ web/app/themes/victcomm2/resources/views/partials/page-header.blade.php
@@ -1,9 +1,5 @@
 <div class="page-header">
   <div class="wrap container">
-    <div class="row">
-      <div class="col-md-8">
-        <h1>{!! App::title() !!}</h1>
-      </div>
-    </div>
+    <h1>{!! App::title() !!}</h1>
   </div>
 </div>
