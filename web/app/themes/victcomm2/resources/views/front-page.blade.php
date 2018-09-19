dba15539b68d3372ce08b36a19b760ada5ca5cca
James Devon <james.devon@digital.justice.gov.uk>
2018-09-19 15:37:28 +0100

    Temporarily disabling newsletter signup form on homepage

--- web/app/themes/victcomm2/resources/views/front-page.blade.php
+++ web/app/themes/victcomm2/resources/views/front-page.blade.php
@@ -53,10 +53,10 @@
 
       <div class="row">
         <div class="col-md-5">
-          <p>Sign up to our newsletter</p>
+{{--          <p>Sign up to our newsletter</p>
           <form action="/">
             <div class="form-group" style="position: relative;"><input id="email" class="form-control" type="text" placeholder="enter your email" /><i class="text-input-button">sign up</i></div>
-          </form>
+          </form>--}}
         </div>
       </div>
 
