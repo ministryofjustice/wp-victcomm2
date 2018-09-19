084495c799fac834bdcf4f8a48ce0410b6ffe5aa
James Devon <james.devon@digital.justice.gov.uk>
2018-09-19 15:17:28 +0100

    Porting existing Twitter feed functionality into homepage

--- web/app/themes/victcomm2/resources/views/front-page.blade.php
+++ web/app/themes/victcomm2/resources/views/front-page.blade.php
@@ -29,62 +29,36 @@
   <div class="highlighted">
     <div class="wrap container">
 
-        <div class="row">
-          <div class="col-md-7">
-            @include('partials.latest-news')
-          </div>
-          <div class="col-md-6"></div>
+      <div class="row">
+        <div class="col-md-7 col-md-push-7">
+          @include('partials.latest-news')
         </div>
-
-        <div class="row">
-          <div class="col-md-5">
-            <h2>Latest Tweets</h2>
-
-            <div class="tweet">
-              <div class="fa fa-twitter" ></div>
-              <div><strong>@VictimsComm retweeted<br/>
-              @RefugeCharity | Aug 9</strong></div>
-              <a>The National Domestic Violence Helpline, which we run in partnership with @womensaid,
-                is open 24 hours a day, seven days a week. Please call 0808 2000 247.
-              </a>
-            </div>
-
-            <div class="tweet">
-              <div class="fa fa-twitter" ></div>
-              <div><strong>@VictimsComm | Aug 9</strong></div>
-              <a>This is brilliant news and hopefully is a major step towards securing truly sustainable long-term funding for refuges.
-                Pleased that @mhclg have listened to those in the know @womensaid @RefugeCharity https://victimscommissioner.org.uk/vc-makes-14-…
-              </a>
-            </div>
-
-            <div class="tweet">
-              <div class="fa fa-twitter" ></div>
-              <div><strong>@VictimsComm | Aug 9</strong></div>
-              <a>This is brilliant news and hopefully is a major step towards securing truly sustainable long-term funding for refuges.
-                Pleased that @mhclg have listened to those in the know @womensaid @RefugeCharity https://victimscommissioner.org.uk/vc-makes-14-…
-              </a>
-            </div>
-          </div>
+        <div class="col-md-5 col-md-pull-5">
+          @include('partials.latest-tweets')
         </div>
+      </div>
 
-        <div class="row">
-          <div class="col-md-5">
-            <hr/>
-            <h2>Make a complaint</h2>
-            Contact a criminal justice agency directly if you want to <a href="/make-a-complaint/">make a complaint</a>.
-          </div>
+      <div class="row">
+        <div class="col-md-5">
+          <hr/>
+          <h2>Make a complaint</h2>
+          Contact a criminal justice agency directly if you want to <a href="/make-a-complaint/">make a complaint</a>.
         </div>
+        <div class="col-md-7">
 
-        <hr/>
-
-        <div class="row">
-          <div class="col-md-5">
-            <p>Sign up to our newsletter</p>
-            <form action="/">
-              <div class="form-group" style="position: relative;"><input id="email" class="form-control" type="text" placeholder="enter your email" /><i class="text-input-button">sign up</i></div>
-            </form>
-          </div>
         </div>
+      </div>
+
+      <hr/>
+
+      <div class="row">
+        <div class="col-md-5">
+          <p>Sign up to our newsletter</p>
+          <form action="/">
+            <div class="form-group" style="position: relative;"><input id="email" class="form-control" type="text" placeholder="enter your email" /><i class="text-input-button">sign up</i></div>
+          </form>
+        </div>
+      </div>
 
     </div>
   </div>
