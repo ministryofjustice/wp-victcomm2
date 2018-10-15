eab248d81bfbc4c8ba530db132d0a5bb2854e661
James Devon <james.devon@digital.justice.gov.uk>
2018-10-15 16:04:44 +0100

    Removing commented out code

--- web/app/themes/victcomm2/resources/views/partials/page-header--archive-annual-report.blade.php
+++ web/app/themes/victcomm2/resources/views/partials/page-header--archive-annual-report.blade.php
@@ -1,6 +1,5 @@
 <div class="page-header page-header--archive-annual-report">
   <div class="wrap container">
     <h1>{!! post_type_archive_title()!!}</h1>
-    {{--<h2>page-header--archive-annual-report.blade.php</h2>--}}
   </div>
 </div>
--- web/app/themes/victcomm2/resources/views/partials/page-header--archive-special-report.blade.php
+++ web/app/themes/victcomm2/resources/views/partials/page-header--archive-special-report.blade.php
@@ -1,6 +1,5 @@
 <div class="page-header page-header--archive-special-report">
   <div class="wrap container">
     <h1>{!! post_type_archive_title()!!}</h1>
-    {{--<h2>page-header--archive-special-report.blade.php</h2>--}}
   </div>
 </div>
