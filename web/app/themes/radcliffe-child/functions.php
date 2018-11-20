340f10e0071ce57fb522e8273322a5c715a16f21
James Devon <james.devon@digital.justice.gov.uk>
2018-11-20 11:00:09 +0000

    Referring to parent style theme correctly in child theme

--- web/app/themes/radcliffe-child/functions.php
+++ web/app/themes/radcliffe-child/functions.php
@@ -4,12 +4,12 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style('google/Merriweather', 'https://fonts.googleapis.com/css?family=Merriweather:400,900i', false, null);
 
-    $parent_style = 'parent-style';
+    $parent_style = 'radcliffe_style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
+
+    wp_enqueue_style( 'radcliffe-child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
+        array( $parent_style )
     );
 }
 
