45d75ba2eb8536041be0f38842b6582c2a94bb1a
James Devon <james.devon@digital.justice.gov.uk>
2018-12-04 15:56:27 +0000

    Creating ACF page summary field
    Specifying that the ACF .json should be stored in mu-plugins/acf-json;
    Styled the .post-summary class;

--- web/app/themes/radcliffe-child/functions.php
+++ web/app/themes/radcliffe-child/functions.php
@@ -13,3 +13,34 @@ function my_theme_enqueue_styles() {
     );
 }
 
+/**
+ * Define where ACF settings file is located
+ *
+ * https://www.advancedcustomfields.com/resources/local-json/
+ */
+function acf_json_save_point( $path )
+{
+	// update path
+	$path = WPMU_PLUGIN_DIR .  '/acf-json';
+
+	return $path;
+}
+add_filter('acf/settings/save_json', __NAMESPACE__ . '\\acf_json_save_point');
+
+/**
+ * ACF schema now loaded from /mu-plugins/acf-json
+ *
+ * See acf_json_save_point for rationale.
+ */
+function acf_json_load_point( $paths )
+{
+	// remove original path
+	unset($paths[0]);
+
+	// append path
+	$paths[] = WPMU_PLUGIN_DIR . '/acf-json';
+
+	return $paths;
+}
+add_filter('acf/settings/load_json', __NAMESPACE__ . '\\acf_json_load_point');
+
