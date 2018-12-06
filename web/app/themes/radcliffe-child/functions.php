e7230470acdd86a95bdb614477aa0dcd33c6e189
James Devon <james.devon@digital.justice.gov.uk>
2018-12-06 14:52:24 +0000

    Removing redundant enqueueing of parent theme css
    The child theme css already declares a dependency on the the parent css,
    so manually enqueuing the parent css is redundant.

--- web/app/themes/radcliffe-child/functions.php
+++ web/app/themes/radcliffe-child/functions.php
@@ -4,12 +4,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style('google/Merriweather', 'https://fonts.googleapis.com/css?family=Merriweather:400,900i', false, null);
 
-    $parent_style = 'radcliffe_style';
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-
-    wp_enqueue_style( 'radcliffe-child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style )
-    );
+    wp_enqueue_style( 'radcliffe_child_style', get_stylesheet_directory_uri() . '/style.css', array( 'radcliffe_style' ) );
 }
+
 
