ddb6df51c330ab73bc821b47c215f583df78c326
James Devon <james.devon@digital.justice.gov.uk>
2019-02-21 15:32:54 +0000

    Fix meeting note archive summary not showing

--- web/app/themes/radcliffe/archive-meeting-notes.php
+++ web/app/themes/radcliffe/archive-meeting-notes.php
@@ -1,8 +1,8 @@
 <?php
 
 $templateData = [
-    'archive-text'       => ($archiveText      = get_field('meeting_notes_archive_text',      'option')) ?  $archiveText      : '',
     'summary'            => ($archiveSummary   = get_field('meeting_notes_archive_summary',   'option')) ?  $archiveSummary   : '',
+    'archive-text'       => ($archiveText      = get_field('meeting_notes_archive_text',      'option')) ?  $archiveText      : '',
     'archive-outro-text' => ($archiveOutroText = get_field('meeting_notes_archive_outro_text','option')) ?  $archiveOutroText : '',
 ];
 
