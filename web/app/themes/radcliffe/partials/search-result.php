291ea8bb89f88e97cecb464e514f0f23bfa25f81
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-02-21 11:32:15 +0000

    Search results: remove type page + date

--- web/app/themes/radcliffe/partials/search-result.php
+++ web/app/themes/radcliffe/partials/search-result.php
@@ -5,19 +5,19 @@ $td = $vcTemplateData;
 ?>
 <div class="search-result section-inner">
 
-    <div class="list-item-meta">
+    <?php if (strtolower($td['post-type-name']) !== 'page') : ?>
+        <div class="list-item-meta">
+            <div class="list-item-meta__type"><?= $td['post-type-name'] ?></div>
 
-        <div class="list-item-meta__type"><?= $td['post-type-name'] ?></div>
+            <?php if (isset($td['date-format'])) : ?>
 
-        <?php if (isset($td['date-format'])) : ?>
+                <div class="list-item-meta__divider">|</div>
+                <div class="list-item-meta__date"> <?= get_the_date($td['date-format']); ?></div>
 
-            <div class="list-item-meta__divider">|</div>
-            <div class="list-item-meta__date"> <?= get_the_date($td['date-format']); ?></div>
+            <?php endif; ?>
 
-        <?php endif; ?>
-
-    </div>
-
+        </div>
+    <?php endif; ?>
     <h2 class="search-result__title">
         <a href="<?php the_permalink(); ?>">
             <?php
@@ -30,9 +30,9 @@ $td = $vcTemplateData;
         </a>
     </h2>
 
-    <?php if ($the_excerpt = get_the_excerpt() ) : ?>
+    <?php if ($the_excerpt = get_the_excerpt()) : ?>
 
-        <div class="search-result__excerpt"><?= $the_excerpt?></div>
+        <div class="search-result__excerpt"><?= $the_excerpt ?></div>
 
     <?php endif; ?>
 
