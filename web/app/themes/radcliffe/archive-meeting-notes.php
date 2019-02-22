b312cb1d9af245e454b943e4f1b7cb1e21e75e4f
James Devon <james.devon@digital.justice.gov.uk>
2019-02-22 14:07:18 +0000

    Show date on meeting note archive page

--- web/app/themes/radcliffe/archive-meeting-notes.php
+++ web/app/themes/radcliffe/archive-meeting-notes.php
@@ -4,6 +4,7 @@ $templateData = [
     'summary'            => ($archiveSummary   = get_field('meeting_notes_archive_summary',   'option')) ?  $archiveSummary   : '',
     'archive-text'       => ($archiveText      = get_field('meeting_notes_archive_text',      'option')) ?  $archiveText      : '',
     'archive-outro-text' => ($archiveOutroText = get_field('meeting_notes_archive_outro_text','option')) ?  $archiveOutroText : '',
+    'date-format'        => get_option( 'date_format' ),
 ];
 
 echo template($templateData, 'report-archive');
