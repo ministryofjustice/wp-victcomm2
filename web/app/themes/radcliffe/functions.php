bbccc61265a728f34d25dcf56934a9ba8cf7a573
James Devon <james.devon@digital.justice.gov.uk>
2019-03-05 16:54:07 +0000

    Reverting image size/aspect for latest images on home page

--- web/app/themes/radcliffe/functions.php
+++ web/app/themes/radcliffe/functions.php
@@ -718,7 +718,10 @@ add_action( 'save_post', function($postId, $post, $update) {
 
 add_action('init', function() {
     add_image_size( 'report', 200);
-    add_image_size( 'archive-news', 200, 200, array( 'center', 'center' ));
+
+    add_image_size( 'archive-news', 600, 337, array( 'center', 'center' ));
+
+    add_image_size( 'archive-news-preview', 200, 200, array( 'center', 'center' ));
 });
 
 function template($data, $slug, $name = '') {
