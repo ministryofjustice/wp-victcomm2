e2bfbc45db88b0c6fa6a5e0887627f9f7948f1fd
Stephanie David <stephdaviduk@gmail.com>
2019-06-30 20:32:29 +0100

    fix two warnings issues

--- web/app/themes/radcliffe/header.php
+++ web/app/themes/radcliffe/header.php
@@ -4,8 +4,8 @@
 
 <head>
 
-    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
 
     <?php wp_head(); ?>
 
