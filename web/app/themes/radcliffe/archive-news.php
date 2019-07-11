af8cc143e9394af9a2e7fd74197d6df2b63efa22
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2019-07-11 13:14:15 +0100

    PSR2 Code Tidy

--- web/app/themes/radcliffe/archive-news.php
+++ web/app/themes/radcliffe/archive-news.php
@@ -7,98 +7,86 @@ global $vcTemplateData;
 global $wp_query;
 
 $td = [
-    'archive-text' => ($archiveText    = get_field('news_archive_text',    'option')) ?  $archiveText    : '',
-    'summary'      => ($archiveSummary = get_field('news_archive_summary', 'option')) ?  $archiveSummary : '',
-    'date-format'  =>  get_option( 'date_format' ),
-    'archive-outro-text' => ($archiveOutroText    = get_field('news_archive_outro_text',    'option')) ?  $archiveOutroText    : '',
+    'archive-text' => ($archiveText = get_field('news_archive_text', 'option')) ? $archiveText : '',
+    'summary' => ($archiveSummary = get_field('news_archive_summary', 'option')) ? $archiveSummary : '',
+    'date-format' => get_option('date_format'),
+    'archive-outro-text' => ($archiveOutroText = get_field('news_archive_outro_text', 'option')) ? $archiveOutroText : '',
 ];
 
 $placeholderCounter = 0;
 
 ?>
 
-    <main class="content" id="maincontent">
+<main class="content" id="maincontent">
 
-        <div <?php post_class('post single'); ?>>
+    <div <?php post_class('post single'); ?>>
 
-            <div class="post-header section">
+        <div class="post-header section">
 
-                <div class="post-header-inner section-inner">
+            <div class="post-header-inner section-inner">
 
-                    <h2 class="post-title">Latest news</h2>
+                <h2 class="post-title">Latest news</h2>
 
-                    <?php if (isset($td['summary'])) : ?>
-                        <p class="post-summary"><?= $td['summary'] ?></p>
-
-                    <?php endif; ?>
-
-                </div>
-
-            </div>
-
-            <?php if (isset($td['archive-text'])) : ?>
-                <div class="post-content section-inner">
-
-                    <div class="archive__content"><?= $td['archive-text'] ?></div>
-
-                </div>
-
-            <?php endif; ?>
-
-            <div class="section-inner archive-news">
-
-                <ul class="archive-news__list">
-
-                    <?php while (have_posts()) :
-                        the_post();
-                        ?>
-
-                        <li class="archive-news__item">
-
-                            <a class="archive-news__item-button" href="<?= the_permalink() ?>">
-
-                                <?= getThumbnail($placeholderCounter) ?>
-
-                                <div class="archive-news__item-date"><?= get_the_date( get_option('date_format') ); ?></div>
-
-                                <h2 class="archive-news__item-title"><?php the_title(); ?></h2>
-
-                                <p class="archive-news__item-summary">
-
-                                    <?php if (!$pageSummary = get_field('summary')) {
-
-                                        $pageSummary = get_the_excerpt();
-
-                                    }
-                                    echo wp_trim_words($pageSummary, 20);
-                                    ?>
-                                </p>
-
-                            </a>
-
-                        </li>
-
-                    <?php endwhile; ?>
-
-                </ul>
-
-                <?php if ($wp_query->max_num_pages > 1) : ?>
-                    <div class="archive-news__nav">
-
-                        <?php echo get_previous_posts_link('&laquo; ' . __('Previous news', 'radcliffe')); ?>
-
-                        <?php echo get_next_posts_link(__('More news', 'radcliffe') . ' &raquo;'); ?>
-
-                    </div>
+                <?php if (isset($td['summary'])) : ?>
+                    <p class="post-summary"><?= $td['summary'] ?></p>
 
                 <?php endif; ?>
 
             </div>
 
-            <?php if (isset($td['archive-outro-text'])) : ?>
-                <div class="post-content section-inner">
+        </div>
 
-                    <div class="archive__content"><?= $td['archive-outro-text'] ?></div>
+        <?php if (isset($td['archive-text'])) : ?>
+            <div class="post-content section-inner">
+
+                <div class="archive__content"><?= $td['archive-text'] ?></div>
+
+            </div>
+
+        <?php endif; ?>
+
+        <div class="section-inner archive-news">
+
+            <ul class="archive-news__list">
+
+                <?php while (have_posts()) :
+                    the_post();
+                    ?>
+
+                    <li class="archive-news__item">
+
+                        <a class="archive-news__item-button" href="<?= the_permalink() ?>">
+
+                            <?= getThumbnail($placeholderCounter) ?>
+
+                            <div class="archive-news__item-date"><?= get_the_date(get_option('date_format')); ?></div>
+
+                            <h2 class="archive-news__item-title"><?php the_title(); ?></h2>
+
+                            <p class="archive-news__item-summary">
+
+                                <?php if (!$pageSummary = get_field('summary')) {
+                                    $pageSummary = get_the_excerpt();
+
+                                }
+                                echo wp_trim_words($pageSummary, 20);
+                                ?>
+                            </p>
+
+                        </a>
+
+                    </li>
+
+                <?php endwhile; ?>
+
+            </ul>
+
+            <?php if ($wp_query->max_num_pages > 1) : ?>
+                <div class="archive-news__nav">
+
+                    <?php echo get_previous_posts_link('&laquo; ' . __('Previous news', 'radcliffe')); ?>
+
+                    <?php echo get_next_posts_link(__('More news', 'radcliffe') . ' &raquo;'); ?>
 
                 </div>
 
@@ -106,6 +94,17 @@ $placeholderCounter = 0;
 
         </div>
 
-    </main>
+        <?php if (isset($td['archive-outro-text'])) : ?>
+            <div class="post-content section-inner">
+
+                <div class="archive__content"><?= $td['archive-outro-text'] ?></div>
+
+            </div>
+
+        <?php endif; ?>
+
+    </div>
+
+</main>
 
 <?php get_footer(); ?>
